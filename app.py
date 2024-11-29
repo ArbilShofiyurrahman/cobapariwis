3cb747123515bd367c1205cf1744117b2058d0ce
ARBIL SHOFIYURRAHMAN <111713911+ArbilShofiyurrahman@users.noreply.github.com>
2024-11-29 21:07:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -26,8 +26,7 @@ vectorizers = {
     "masakan": joblib.load('tfidf_vectorizer_masakan.pkl')
 }
 
-# Load the 'finish_setelah_preprocessing.xlsx' file
-df = pd.read_excel('finish_setelah_preprocessing.xlsx')
+
 
 def main():
     st.title("Sistem Prediksi Aspek dan Sentimen dengan Random Forest")
